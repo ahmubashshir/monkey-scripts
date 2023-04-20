359b047254aea533d3bec6b7d7f575df3b2b6519
Mubashshir <ahmubashshir@gmail.com>
2023-04-21 00:09:50 +0600

    Add gamefaqs single page image fix
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## [Wikipedia Dark](js/wikipedia-dark.user.js)
+## [Fix GameFaqs Single page images](js/gamefaqs-faq-imgs.user.js)
---
+++ js/gamefaqs-faq-imgs.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name           Fix GameFaqs Single page images
+// @version        2
+// @grant          none
+// @run-at         document-end
+// @include        /^https?:\/\/gamefaqs\.gamespot\.com\/.+\/.+\/faqs\/[0-9]*\?(|.*&)(single=1)(|&.*)$/
+// @updateURL      https://ahmubashshir.github.io/monkey-scripts/js/gamefaqs-faq-imgs.user.js
+// ==/UserScript==
+(function () {
+	((func) => {
+		URL.prototype.basename = URL.prototype.basename || func;
+		Location.prototype.basename = Location.prototype.basename ||
+			func;
+	})(function () {
+		"use strict";
+		return this.pathname.split('/').filter(x => x).slice(-1)[0];
+	});
+	((faq) => {
+		document.querySelectorAll('#faqwrap img').forEach((elem) => {
+			let src = new URL(elem.src);
+			if (!src.pathname.startsWith('/ffaq/')) {
+				return;
+			}
+			let idx = src.basename();
+			let path = `/a/faqs/${faq.slice(-2)}/${faq}-${idx}`;
+			elem.src = "", elem.srcset =
+				`${path}.jpg, ${path}.png`;
+			console.log(path);
+		});
+	})(location.basename());
+})();
