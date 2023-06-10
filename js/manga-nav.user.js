a6f8d13250e6c4467b7683b08088fa523c06a769
Mubashshir <ahmubashshir@gmail.com>
2023-06-10 16:31:41 +0600

    manga-nav: Add webtoons.com support
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- js/manga-nav.user.js
+++ js/manga-nav.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name           Keyboard navigation for manga sites [kakalot, nelo]
-// @version        5
+// @name           Keyboard navigation for manga sites [kakalot, nelo, webtoons]
+// @version        6
 // @grant          none
 // @run-at         document-start
 // @include        /^https:\/\/.+manganato\.com\/manga-.+\/chapter-.+/
 // @include        /^https:\/\/mangakakalot.com\/chapter\/.+\/chapter_.*/
+// @include        /^https:\/\/www.webtoons.com\/.+\/episode-.+\/viewer/
 // @updateURL      https://ahmubashshir.github.io/monkey-scripts/js/manga-nav.user.js
 // ==/UserScript==
 (() => {
@@ -19,16 +20,22 @@
 				};
 			};
 
-			if (testhost(/^\w+manganato\.com$/))
+			if (testhost(/^\w+manganato\.com$/)) {
 				return findnav({
 					next: 'a.navi-change-chapter-btn-next',
 					prev: 'a.navi-change-chapter-btn-prev'
 				});
-			else if (testhost(/^mangakakalot\.com$/))
+			} else if (testhost(/^mangakakalot\.com$/)) {
 				return findnav({
 					next: '.btn-navigation-chap > a.back',
 					prev: '.btn-navigation-chap > a.next'
 				});
+			} else if (testhost(/^www\.webtoons\.com$/)) {
+				return findnav({
+					next: 'a.pg_next._nextEpisode',
+					prev: 'a.pg_prev._prevEpisode'
+				});
+			}
 
 			return undefined;
 		};
